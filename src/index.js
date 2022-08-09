13ad7a2a72525c0dbbfe1314655a7cab6ee8c3b1
Luiz Américo Pereira Câmara <camara_luiz@yahoo.com.br>
2022-08-09 10:12:34 -0300

    Remove unused methods

--- src/index.js
+++ src/index.js
@@ -12,29 +12,9 @@ export * from "lit/decorators.js";
 
 export class RawElement extends UpdatingElement {
   /**
-   * Performs element initialization. By default this calls `createRenderRoot`
-   * to create the element `renderRoot` node and captures any pre-set values for
-   * registered properties.
-   */
-  initialize() {
-    super.initialize();
-    this.renderRoot = this.createRenderRoot();
-  }
-
-  /**
-   * Returns the node into which the element should render and by default
-   * creates and returns an open shadowRoot. Implement to customize where the
-   * element's DOM is rendered. For example, to render into the element's
-   * childNodes, return `this`.
-   * @returns {Element|DocumentFragment} Returns a node into which to render.
-   */
-  createRenderRoot() {
-    return this.attachShadow({ mode: "open" });
-  }
-  /**
    * Updates the element. This method reflects property values to attributes
    * and calls `render` to render DOM
-   * * @param _changedProperties Map of changed properties with old values
+   * * @param changedProperties Map of changed properties with old values
    */
   update(changedProperties) {
     super.update(changedProperties);
