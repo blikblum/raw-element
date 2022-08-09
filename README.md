5bb149df43b47e4fa16df04d19c667248102a8c3
Luiz Américo Pereira Câmara <camara_luiz@yahoo.com.br>
2022-08-09 09:32:52 -0300

    Use lit instead of lit-element, remove build step and add example

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # Raw Element
 
-> Raw Element is a base web component without a builtin render
-
+> Raw Element is a base custom element class with reactive properties but without a builtin render
 
 ### Features
 
-&nbsp; &nbsp; ✓ Based on UpdatingElement ([LitElement](https://lit-element.polymer-project.org) base class)<br>
-
+&nbsp; &nbsp; ✓ Based on UpdatingElement ([Lit](lit.dev) base class)<br>
 
 ### Installation
 
@@ -17,35 +15,45 @@ yarn add raw-element
 ### Usage
 
 ```js
-import { RawElement } from 'raw-element'
+import { RawElement } from "raw-element";
 
 export class MyComponent extends RawElement {
   static get properties() {
     return {
-      title: { type: String },      
-    }
+      title: { type: String },
+    };
   }
 
-  render() {    
-    this.innerHTML = `<div>${this.title}</div>`
+  render() {
+    this.renderRoot.innerHTML = `<div>${this.title}</div>`;
   }
 }
 
-customElements.define('my-component', MyComponent)
+customElements.define("my-component", MyComponent);
+```
+
+```html
+<button type="button" onclick="toggleTitle()">Toggle title</button>
+<my-component title="Hello!"></my-component>
+<script>
+  function toggleTitle() {
+    document.querySelector("my-component").title = "New title";
+  }
+</script>
 ```
 
 ### Examples
 
- - https://github.com/blikblum/wc-fontawesome
- - https://github.com/blikblum/pdfmake-utils/blob/master/src/pdf-viewer.js
-
+- https://github.com/blikblum/wc-fontawesome
+- https://github.com/blikblum/pdfmake-utils/blob/master/src/pdf-viewer.js
 
 ### License
 
-Copyright © 2019 Luiz Américo. This source code is licensed under the MIT license found in
+Copyright © 2022 Luiz Américo. This source code is licensed under the MIT license found in
 the [LICENSE.txt](https://github.com/blikblum/raw-element/blob/master/LICENSE.txt) file.
 The documentation to the project is licensed under the [CC BY-SA 4.0](http://creativecommons.org/licenses/by-sa/4.0/)
 license.
 
 ---
+
 Made with ♥ by Luiz Américo
--- src/index.js
+++ src/index.js
@@ -7,41 +7,41 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import { UpdatingElement } from 'lit-element/lib/updating-element.js'
-export * from 'lit-element/lib/decorators.js'
+import { UpdatingElement } from "lit";
+export * from "lit/decorators.js";
 
 export class RawElement extends UpdatingElement {
   /**
- * Performs element initialization. By default this calls `createRenderRoot`
- * to create the element `renderRoot` node and captures any pre-set values for
- * registered properties.
- */
-  initialize () {
-    super.initialize()
-    this.renderRoot = this.createRenderRoot()
+   * Performs element initialization. By default this calls `createRenderRoot`
+   * to create the element `renderRoot` node and captures any pre-set values for
+   * registered properties.
+   */
+  initialize() {
+    super.initialize();
+    this.renderRoot = this.createRenderRoot();
   }
 
   /**
-     * Returns the node into which the element should render and by default
-     * creates and returns an open shadowRoot. Implement to customize where the
-     * element's DOM is rendered. For example, to render into the element's
-     * childNodes, return `this`.
-     * @returns {Element|DocumentFragment} Returns a node into which to render.
-     */
-  createRenderRoot () {
-    return this.attachShadow({ mode: 'open' })
+   * Returns the node into which the element should render and by default
+   * creates and returns an open shadowRoot. Implement to customize where the
+   * element's DOM is rendered. For example, to render into the element's
+   * childNodes, return `this`.
+   * @returns {Element|DocumentFragment} Returns a node into which to render.
+   */
+  createRenderRoot() {
+    return this.attachShadow({ mode: "open" });
   }
   /**
-     * Updates the element. This method reflects property values to attributes
-     * and calls `render` to render DOM
-     * * @param _changedProperties Map of changed properties with old values
-     */
-  update (changedProperties) {
-    super.update(changedProperties)
-    this.render()
+   * Updates the element. This method reflects property values to attributes
+   * and calls `render` to render DOM
+   * * @param _changedProperties Map of changed properties with old values
+   */
+  update(changedProperties) {
+    super.update(changedProperties);
+    this.render();
   }
   /**
-     * Invoked on each update to perform rendering tasks.
-     */
-  render () { }
+   * Invoked on each update to perform rendering tasks.
+   */
+  render() {}
 }
